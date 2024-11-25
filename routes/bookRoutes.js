03383bf388c88c1c729d7764555ccfd61ed75b83
MrBarker47 <jebarker7@gmail.com>
2024-11-25 13:36:30 -0500

    Update files

--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -1,32 +1,23 @@
 const express = require('express');
 const router = express.Router();
 
-
-
-
-// Define routes here
+//Define routes here
 app.post('/api/books', (req, res) => {
- 
-})
 
+});
 app.get('/api/books', (req, res) => {
 
-})
+});
+app.get('/api/books', (req, res) => {
 
-app.get('/api/books/:id', (req, res) => {
+});
+app.put('/api/books', (req, res) => {
 
-})
+});
+app.delete('/api/books', (req, res) => {
 
-app.get('/api/books/:id', (req, res) => {
+});
 
-})
 
-app.post('/api/books/:id', (req, res) => {
-
-})
-
-app.delete('/api/books/:id', (req, res) => {
-
-})
 
 module.exports = router;
